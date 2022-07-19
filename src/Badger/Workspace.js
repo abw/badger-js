026cdb3823f7c4732dcd1accccce5f8d49f1b929
Andy Wardley <abw@wardley.org>
2022-07-19 10:01:07 +0100

    Added Badger/Library to Badger/Workspace

--- src/Badger/Workspace.js
+++ src/Badger/Workspace.js
@@ -2,9 +2,12 @@ import { dir } from "./Filesystem/Directory.js";
 import { requiredParam } from "./Utils/Params.js";
 import { hasValue } from "./Utils/Misc.js";
 import { addDebug } from "./Utils/Debug.js";
-import { config } from "./Config.js";
+import { Config } from "./Config.js";
+import { Library } from "./Library.js";
 
 const defaults = {
+  library: {
+  },
   config: {
     dir: 'config',
   }
@@ -14,12 +17,14 @@ export class Workspace {
     const rootDir = dir(requiredParam(props, 'dir'));
     const cfgDir  = rootDir.dir(props.config?.dir || defaults.config.dir);
     const cfgOpts = { ...defaults.config, ...(props.config||{}), dir: cfgDir };
-    const cfgObj  = config(cfgOpts);
+    const config  = new Config(cfgOpts);
+    const libOpts = { ...defaults.library, ...(props.library||{}), root: rootDir };
+    const library = new Library(libOpts);
 
     this.state = {
-      rootDir:   rootDir,
-      configDir: cfgDir,
-      config:    cfgObj
+      rootDir,
+      config,
+      library
     }
 
     addDebug(this, props.debug, props.debugPrefix, props.debugColor);
@@ -29,8 +34,20 @@ export class Workspace {
   dir(path, options) {
     this.debug("dir(%s, %o)", path, options);
     return hasValue(path)
-      ? this.state.rootDir(path, options)
+      ? this.state.rootDir.dir(path, options)
       : this.state.rootDir;
+  }
+  file(path, options) {
+    this.debug("file(%s, %o)", path, options);
+    return this.state.rootDir.file(path, options)
+  }
+  read(path, options) {
+    this.debug("read(%s, %o)", path, options);
+    return this.file(path, options).read();
+  }
+  write(path, data, options) {
+    this.debug("write(%s, %o, %o)", path, data, options);
+    return this.file(path, options).write(data);
   }
   configDir(path, options) {
     this.debug("configDir(%s, %o)", path, options);
@@ -43,6 +60,9 @@ export class Workspace {
     return hasValue(uri)
       ? this.state.config.config(uri, defaults)
       : this.state.config;
+  }
+  async lib(uri) {
+    return this.state.library.lib(uri);
   }
 }
 
