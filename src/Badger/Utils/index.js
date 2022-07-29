5d0572afdfe6b1ea50676d7f8e34cb30960c0d07
Andy Wardley <abw@wardley.org>
2022-07-29 12:33:33 +0100

    Added Options to Utils/index.js

--- src/Badger/Utils/index.js
+++ src/Badger/Utils/index.js
@@ -2,6 +2,7 @@ export * from './Color.js';
 export * from './DataPath.js';
 export * from './Debug.js';
 export * from './Misc.js';
+export * from './Options.js';
 export * from './Params.js';
 export * from './Prompt.js';
 export * from './Text.js';
