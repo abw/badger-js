1eb653eb96ce0dc94144ef9e7d1ac6c06a63e15c
Andy Wardley <abw@wardley.org>
2023-03-06 19:14:33 +0000

    Updated rollup and friends

--- rollup.config.js
+++ rollup.config.js
@@ -1,59 +1,12 @@
-import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser'
-import pkg from './package.json';
-
-// Silence circular dependency warnings
-const ignoreWarnings = {
-  'Circular dependency: src/Badger/Filesystem/File.js -> src/Badger/Filesystem/Directory.js -> src/Badger/Filesystem/File.js': true,
-  'Circular dependency: src/Badger/Filesystem/Directory.js -> src/Badger/Filesystem/File.js -> src/Badger/Filesystem/Directory.js': true,
-};
-
-const onwarn = (warning, warn) => {
-  if (
-    warning.code === 'CIRCULAR_DEPENDENCY'
-    && ignoreWarnings[warning.message]
-  ) {
-    return
-  }
-  warn(warning);
-}
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import pkg from './package.json' assert { type: 'json' };
 
 export default [
-  //// browser-friendly UMD build
-  //{
-  //  input: 'src/Badger.js',
-  //  output: {
-  //    name: 'badger',
-  //    file: pkg.browser,
-  //    format: 'umd',
-  //    exports: 'named'
-  //  },
-  //  plugins: [
-  //    resolve(),
-  //    commonjs()
-  //  ]
-  //},
-
   // CommonJS (for Node) and ES module (for bundlers) build.
   {
     input: 'src/Badger.js',
-    plugins: [
-      resolve({
-        extensions: ['.js', '.jsx'],
-      }),
-      commonjs()
-    ],
-    external: [
-      "node:fs",
-      "node:path",
-      "node:process",
-      "node:fs/promises",
-      "js-yaml",
-      "commander",
-      "prompts"
-    ],
-    onwarn,
     output: [
       {
         file: pkg.main,
@@ -69,6 +22,21 @@ export default [
         exports: 'named',
         plugins: [terser()]
       }
-    ]
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs()
+    ],
+    external: [
+      "@abw/badger-codecs",
+      "@abw/badger-filesystem",
+      "node:fs",
+      "node:path",
+      "node:process",
+      "node:fs/promises",
+      "js-yaml",
+      "commander",
+      "prompts"
+    ],
   }
 ];
